11c3ca83d1567af20eb300b2c922214f16a55c38
Rayyan Khattak <linkcode25@gmail.com>
2026-08-01 21:30:19 +0200

    Fixed About animation

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -34,7 +34,7 @@ const About = () => {
     return (
         <>
             <div ref={aboutRef} className="relative z-20 h-screen bg-white rounded-tl-[60px] rounded-tr-[60px]">
-                <div className="flex justify-center items-center
+                <div className="about-text flex justify-center items-center
                 main-container py-4 lg:py-12 h-full font-heading text-black text-2xl md:text-3xl
                 xl:text-5xl leading-tight">
                     Hello this is me I will write somehting
