d79f02b4952561006c94f4f76934d240fc9a5dcd
Rayyan Khattak <linkcode25@gmail.com>
2026-08-02 11:08:22 +0200

    Added animation to work and footer

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import GradientButton from "./GradientButton.jsx";
+import {gsap} from 'gsap'
+import {useGSAP} from "@gsap/react";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger)
 
 const Cta = () => {
+
+    const ctaRef = useRef(null);
+
+    useGSAP(() => {
+        gsap.from(ctaRef.current, {
+            backgroundColor: "#fff",
+            color: "#000",
+            scrollTrigger: {
+                trigger: ctaRef.current,
+                start: "center bottom",
+                end: "90% bottom",
+                scrub: 1,
+            }
+        })
+    });
+
     return (
         <>
-            <div>
+            <div ref={ctaRef}>
                 <div className="main-container py-20 lg:py-29 h-full flex flex-col gap-8
                 justify-center items-center">
                     <h4 className="max-w-6xl text-2xl lg:text-5xl text-center leading-tight">Freelance projects, collaborations and full time opportunities.
--- src/sections/Work.jsx
+++ src/sections/Work.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import GradientButton from "../components/GradientButton.jsx";
 import projects from "../components/ProjectsData.jsx";
+import {useGSAP} from "@gsap/react";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+import {gsap} from 'gsap'
+
+
+gsap.registerPlugin(useGSAP, ScrollTrigger)
 
 const Work = () => {
+
+    const workRef = useRef(null);
+    const projectRef = useRef(null);
+
+    useGSAP(() => {
+        const projectsWidth = projectRef.current.scrollWidth;
+        const scrollDistance = projectsWidth - window.innerWidth;
+
+        gsap.to(projectRef.current, {
+            x: -scrollDistance,
+            ease: "linear",
+            scrollTrigger: {
+                trigger: workRef.current,
+                start: "center center",
+                end: () => `+=${projectsWidth}`,
+                pin: true,
+                scrub: 1,
+                anticipatePin: 1,
+                invalidateOnRefreshH: true,
+            }
+        })
+    })
+
     return (
         <>
-            <div className="h-screen bg-white text-black py-24 lg:py-40">
+            <div ref={workRef} className="h-screen bg-white text-black py-24 lg:py-40">
                 <div className="main-container pb-8 lg:pb-12 flex max-md:flex-col gap-6 justify-between
                 items-start md:items-end">
                     <div className="max-w-xl">
@@ -19,18 +48,21 @@ const Work = () => {
                     />
                 </div>
 
-                <div className="flex gap-4 lg:gap-8 ms-4 lg:ms-[40%] mt-6">
-                    {projects.map(({id, name, image, link}) => (
-                        <a key={id} href={link} className="relative rounded-2xl w-full min-w-[340px]
+                <div ref={projectRef}>
+                    <div className="flex gap-4 lg:gap-8 ms-4 lg:ms-[40%] mt-6">
+                        {projects.map(({id, name, image, link}) => (
+                            <a key={id} href={link} className="relative rounded-2xl w-full min-w-[340px]
                         lg:min-w-xl h-64 lg:h-96 block overflow-hidden group]:">
-                            <img src={image} alt="" className="w-full h-full object-cover transition-transform
+                                <img src={image} alt="" className="w-full h-full object-cover transition-transform
                             duration-300 group-hover:scale-105" />
-                            <span className="absolute top-4 right-4 bg-black text-white
+                                <span className="absolute top-4 right-4 bg-black text-white
                             uppercase leading-[1.4] font-heading px-5 py-1 rounded-full text-sm lg:text-lg">{name}</span>
-                        </a>
-                    ))}
+                            </a>
+                        ))}
 
+                    </div>
                 </div>
+
             </div>
         </>
     )
