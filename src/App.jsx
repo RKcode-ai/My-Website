c8a2fd200c3781f35e31fb94360daac05baff54d
Rayyan Khattak <linkcode25@gmail.com>
2026-08-02 12:04:30 +0200

    Implemented the structure of projects page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import Navbar from "./components/Navbar.jsx";
 import Home from "./pages/Home.jsx";
 import Footer from "./components/Footer.jsx";
+import {Routes, Route} from "react-router-dom";
+import Projects from "./pages/Projects.jsx";
 
 const App = () => {
     return (
        <>
            <Navbar />
-           <Home />
+           <Routes>
+               <Route path="/" element={<Home />} />
+               <Route path="/projects" element={<Projects />} />
+           </Routes>
            <Footer />
        </>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, {useRef, useState} from 'react'
 import Logo from '../assets/images/logo.svg'
 import {gsap} from 'gsap'
 import {useGSAP} from "@gsap/react";
+import {Link} from "react-router-dom";
 
 gsap.registerPlugin(useGSAP)
 
@@ -37,9 +38,9 @@ const Navbar = () => {
 
             <div className={`fixed z-20 inset-0 bg-black text-white flex flex-col items-center justify-center
                 text-3xl gap-8 transition-transform duration-500 ${menuOpen ? `translate-y-0 `: `-translate-y-full`} `} onClick={() => setMenuOpen(false)}>
-                <a href="/" className={"menu-link"}>Home</a>
-                <a href="/" className={"menu-link"}>Projects</a>
-                <a href="/" className={"menu-link"}>Contact</a>
+                <Link to="/" className={"menu-link"}>Home</Link>
+                <Link to="/projects" className={"menu-link"}>Projects</Link>
+                <Link to="mailto:rkhattak042@gmail.com" className={"menu-link"}>Contact</Link>
             </div>
         </>
     )
